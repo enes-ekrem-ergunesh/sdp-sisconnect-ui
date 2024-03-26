c3b9fa78363cb7a6b59353cf4bdd29023d7a400e
Enes Ekrem Ergunesh <enes.ekrem.ergunesh@gmail.com>
2024-03-26 13:22:00 +0200

    User service: - Prepare to request login

--- src/app/services/sis-connect/user/user.service.ts
+++ src/app/services/sis-connect/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {ConfigService} from "../config/config.service";
 import {BasicHttpResponse} from "../../../interfaces/sis-connect/basic-http-response/basic-http-response";
+import {Token} from "../../../interfaces/sis-connect/user/token";
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class UserService {
 
   getUserHello() {
     return this.http.get<BasicHttpResponse>(this.configService.getApiUrl() + '/user/hello');
+  }
+
+  login(email: string, password: string, rememberMe: boolean) {
+    return this.http.post<Token>(this.configService.getApiUrl() + '/user/login', {email, password, rememberMe});
   }
 }
