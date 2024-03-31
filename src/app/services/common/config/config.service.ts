74f68609c5fe1890025f51a4eba6a4891cfb11cc
Enes Ekrem Ergunesh <enes.ekrem.ergunesh@gmail.com>
2024-03-31 09:16:41 +0200

    Update error handler (config.service) - if error is 401 (unauthorized), redirect to login page

--- src/app/services/common/config/config.service.ts
+++ src/app/services/common/config/config.service.ts
@@ -3,7 +3,8 @@ import {Platform} from "@ionic/angular";
 import {environment} from "../../../../environments/environment";
 import {HttpErrorResponse} from "@angular/common/http";
 import {throwError} from "rxjs";
-import {AlertService} from "../alert/alert.service"; // change the path for dev/prod
+import {AlertService} from "../alert/alert.service";
+import {Router} from "@angular/router"; // change the path for dev/prod
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,7 @@ export class ConfigService {
   constructor(
     public platform: Platform,
     private alertService: AlertService,
+    private router: Router,
   ) {
     if (environment.production) {
       // declare apiUrl for production environment
@@ -40,18 +42,29 @@ export class ConfigService {
      *
      * @param {HttpErrorResponse}
      * */
-    this.alertService.createAlert(error.status, "An error occurred!")
-    console.log('Error occurred with status:', error.error.message);
-    if (error.status === 0) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong.
-      console.error(
-        `Backend returned code ${error.status}, body was: `, error.error);
+    // console.log('Error occurred with status:', error.error.message);
+
+    switch (error.status) {
+      case 0: // A client-side or network error occurred. Handle it accordingly.
+        this.alertService.createAlert(error.status, 'An error occurred:' + error.error)
+        break;
+      case 401: // Unauthorized error; redirect to login page
+        this.alertService.createAlert(error.status, "Authorization is missing! Redirecting to login page...")
+        // sleep 2 seconds before redirecting to login page
+        setTimeout(() => {
+          this.router.navigate(['login']).then(() => console.log('Redirecting to login page...'));
+        }, 2000);
+        break;
+      default:
+        try {
+          this.alertService.createAlert(error.status, error.error.message)
+        }
+        catch (e) {
+          this.alertService.createAlert(error.status, "An error occurred: " + error.error)
+        }
     }
+
     // Return an observable with a user-facing error message.
-    return throwError(() => new Error('Something bad happened; please try again later.'));
+    return throwError(() => new Error('Something bad happened!'));
   }
 }
