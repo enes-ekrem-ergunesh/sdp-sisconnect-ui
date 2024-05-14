9723ac0a4a04173356c7cfa945df31427cf42c1e
Enes Ekrem Ergunesh <enes.ekrem.ergunesh@gmail.com>
2024-05-14 14:29:18 +0300

    Update Angular CLI & Ionic CLI

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,9 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'sdp-sisconnect-ui',
-  webDir: 'www',
-  server: {
-    androidScheme: 'https'
-  }
+  webDir: 'www'
 };
 
 export default config;
