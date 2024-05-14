7074aa01b18c4f8b69057bc38b4b3d13d01426c8
Enes Ekrem Ergunesh <enes.ekrem.ergunesh@gmail.com>
2024-05-14 18:43:36 +0300

    Completed navigation between main pages

--- src/app/ui/messages/messages.module.ts
+++ src/app/ui/messages/messages.module.ts
@@ -7,14 +7,16 @@ import { IonicModule } from '@ionic/angular';
 import { MessagesPageRoutingModule } from './messages-routing.module';
 
 import { MessagesPage } from './messages.page';
+import {CustomIonSearchbarModule} from "../common/custom-ion-searchbar/custom-ion-searchbar.module";
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    MessagesPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        MessagesPageRoutingModule,
+        CustomIonSearchbarModule
+    ],
   declarations: [MessagesPage]
 })
 export class MessagesPageModule {}
