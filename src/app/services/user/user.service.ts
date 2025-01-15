cb897031b6c6215d2afd8866ff2b5cbf6284a9f6
Enes Ekrem Ergunesh <enes.ekrem.ergunesh@gmail.com>
2025-01-16 00:07:14 +0100

    Setup user service & Add method to get the current user0

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import {environment} from "../../../environments/environment";
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
+import {AuthService} from "../auth/auth.service";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  api_url = environment.api;
+
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private authService: AuthService
+  ) { }
+
+  async getCurrentUser() {
+    const headers = await this.authService.getAuthorization()
+    if (headers.Authorization === null) {
+      return this.http.get(this.api_url + '/tokens/')
+    }
+    return this.http.get(this.api_url + '/users/self/', {headers: headers});
+  }
+}
